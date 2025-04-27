2d38f5af332adb278201e4910070b5a300933f5f
lobnaSaleh <lobna.saleh2003@gmail.com>
2025-04-27 17:30:41 +0300

    API

---
+++ CoursesManagementSystem/Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CoursesManagementSystem.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursesManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public APIController(IUnitOfWork _unitOfWork, IMapper _mapper)
+        {
+            this._unitOfWork = _unitOfWork;
+            this._mapper = _mapper;
+        }
+
+
+       
+    }
+}
